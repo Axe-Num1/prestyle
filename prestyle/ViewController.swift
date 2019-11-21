087caf3a58e9ba961de54350bd0e580de7389e2a
KangMinseok <qbq5000@gmail.com>
2019-11-21 10:50:07 +0900

    init: xcode project

---
+++ prestyle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prestyle
+//
+//  Created by 강민석 on 2019/11/21.
+//  Copyright © 2019 강민석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
